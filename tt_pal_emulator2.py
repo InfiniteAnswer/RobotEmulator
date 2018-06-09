04d2353ca488d5bf865aa2bd87f7b3ead46d3993
InfiniteAnswer <sampervictor@gmail.com>
2018-06-09 20:52:49 +0200

    4 axis controller with threading - stable

--- tt_pal_emulator2.py
+++ tt_pal_emulator2.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Lock
 from time import sleep, ctime, asctime
 
 
@@ -8,196 +8,179 @@ class Serial:
         self.baudrate = baudrate
         self.timeout = timeout
         self.target = "none"  # Options are "tt", "pal", or "controller"
+        # self.lock = Lock()
 
         # Create dictionaries of valid commands and the time (seconds) they take to execute
-        self.tt_commands = {"233": 1,  # ttHome
+        self.tt_commands = {"233": 10,  # ttHome
                             "234": 2,  # ttMoveAbs
                             "232": 0.5,  # ttServo
-                            "212": 0.1,  # ttMoving Query
-                            "238": 0.1}  # ttStop
+                            "212": 2,  # ttMoving Query
+                            "238": 3}  # ttStop
 
         self.pal_commands = {":01060D001010CC\r\n": 1,  # palHome
                              ":01060D030001E8\r\n": 1,  # palSet_position_1
                              ":01060D030002E7\r\n": 1,  # palSet_position_2
                              ":01060D001000DC\r\n": 1,  # palCSTR_off
-                             ":01060D001008D4\r\n": 5,  # palCSTR_on
+                             ":01060D001008D4\r\n": 2,  # palCSTR_on
                              ":01039007000164\r\n": 1,  # palQuery_moving
                              ":01039005000166\r\n": 1,  # palQuery_home
                              ":01050427FF00D0\r\n": 1}  # enableModbus
 
-        if port=="COM3":
+        self.command_buffer = list()
+        self.process_time = list()
+        self.returned_message = list()
+        self.axis_busy_state = list()
+        self.ready_for_next_command = list()
+        self.thread_list = list()
+
+        if port == "COM3":
             self.target = "pal"
-            self.command_buffer_ax1 = list()
-            self.returned_messages_ax1 = list()
-            self.busy_ax1 = False
-            self.ready_for_next_command_ax1 = True
-            self.process_time_ax1 = 0
-            th1 = Thread(target=self.ax1_server)
-            th1.daemon = True
-            th1.start()   
-        if port=="COM4":
+            self.command_buffer.append(list())
+            self.axis_busy_state.append(False)
+            self.ready_for_next_command.append(True)
+            self.process_time.append(0)
+            th = Thread(target=self.axis_server, args=(0,))
+            th.daemon = True
+            self.thread_list.append(th)
+            self.thread_list[0].start()
+
+        if port == "COM4":
             self.target = "tt"
-            self.command_buffer_ax1 = list()
-            self.returned_messages_ax1 = list()
-            self.busy_ax1 = False
-            self.ready_for_next_command_ax1 = True
-            self.process_time_ax1 = 0
-            self.command_buffer_ax2 = list()
-            self.returned_messages_ax2 = list()
-            self.busy_ax2 = False
-            self.ready_for_next_command_ax2 = True
-            self.process_time_ax2 = 0
-            self.command_buffer_ax3 = list()
-            self.returned_messages_ax3 = list()
-            self.busy_ax3 = False
-            self.ready_for_next_command_ax3 = True
-            self.process_time_ax3 = 0
-            th1 = Thread(target=self.ax1_server)
-            th1.daemon = True
-            th2 = Thread(target=self.ax2_server)
-            th2.daemon = True
-            th3 = Thread(target=self.ax3_server)
-            th3.daemon = True
-            th1.start()
-            th2.start()
-            th3.start()
-        if port=="COM5":
+            for i in range(3):
+                self.command_buffer.append(list())
+                self.axis_busy_state.append(False)
+                self.ready_for_next_command.append(True)
+                self.process_time.append(0)
+                th = Thread(target=self.axis_server, args=(i,))
+                th.daemon = True
+                self.thread_list.append(th)
+                self.thread_list[i].start()
+
+        if port == "COM5":
             self.target = "controller"
-        
 
     def write(self, msg):
         # If command is valid, add to command buffer
+        axis = 0
         if self.target == "tt":
             msg_command = msg[3:6]
             if msg_command in self.tt_commands:
-                # check to see which axis and behave apprpriately...
-                if ...
-                
-                self.command_buffer.append((msg_command,
-                                            list(self.tt_commands.values())[
-                                                list(self.tt_commands.keys()).index(msg_command)]))
-                self.returned_messages.append(msg_command)
+                # check to see which axis and behave appropriately...
+                if msg[7] == "1":
+                    axis = 0
+                if msg[7] == "2":
+                    axis = 1
+                if msg[7] == "4":
+                    axis = 2
+
+                if msg_command != "212":
+                    self.command_buffer[axis].append((msg_command,
+                                                 list(self.tt_commands.values())[
+                                                     list(self.tt_commands.keys()).index(msg_command)]))
+                    self.returned_message.append(msg_command)
                 # Remove all query commands from command_buffer
-                if msg_command == "212":
-                    del self.command_buffer[-1]
+                else:
+                    self.returned_message.append(self.axis_busy_state[axis])
             else:
-                self.returned_messages.append("invalid tt command")
+                self.returned_message.append("invalid tt command")
         if self.target == "pal":
             msg_command = msg
             if msg_command in self.pal_commands:
-                self.command_buffer.append((msg_command,
-                                            list(self.pal_commands.values())[
-                                                list(self.pal_commands.keys()).index(msg_command)]))
-                self.returned_messages.append(msg_command)
+                if msg_command != ":01039007000164\r\n":
+                    self.command_buffer[0].append((msg_command,
+                                                list(self.pal_commands.values())[
+                                                    list(self.pal_commands.keys()).index(msg_command)]))
+                    self.returned_message.append(msg_command)
                 # Remove all query commands from command_buffer
-                if msg_command == ":01039007000164\r\n":
-                    del self.command_buffer[-1]
+                else:
+                    self.returned_message.append(self.axis_busy_state[0])
             else:
-                self.returned_messages.append("invalid pal command")
-
+                self.returned_message.append("invalid pal command")
 
     def readline(self):
-        msg = self.returned_messages[0]
+        msg = self.returned_message[0]
         if msg == "212" or msg == ":01039007000164\r\n":
-            msg = self.busy
-        del self.returned_messages[0]
+            msg = self.returned_message[1]
+        del self.returned_message[0:1]
         return msg
 
 
-    def ax1_server(self):
+    def axis_server(self, axis=0):
         while True:
-            if self.command_buffer_ax1 and self.ready_for_next_command_ax1:
-                self.busy_ax1 = True
-                self.ready_for_next_command_ax1 = False
-                self.process_time_ax1 = self.command_buffer_ax1[0][1]
-                del self.command_buffer_ax1[0]
-                print(
-                    "\nStarting new {} task for duration {} at time {}\n".format(self.target, self.process_time_ax1, ctime()))
-                sleep(self.process_time_ax1)
-                self.ready_for_next_command_ax1 = True
-                if not self.command_buffer_ax1:
-                    self.busy_ax1 = False
-                    
-    
-    def ax2_server(self):
-        while True:
-            if self.command_buffer_ax2 and self.ready_for_next_command_ax2:
-                self.busy_ax2 = True
-                self.ready_for_next_command_ax2 = False
-                self.process_time_ax2 = self.command_buffer_ax2[0][1]
-                del self.command_buffer_ax2[0]
-                print(
-                    "\nStarting new {} task for duration {} at time {}\n".format(self.target, self.process_time_ax2, ctime()))
-                sleep(self.process_time_ax2)
-                self.ready_for_next_command_ax2 = True
-                if not self.command_buffer_ax2:
-                    self.busy_ax2 = False
+            if self.command_buffer[axis]: print("command Buffer for axis {} is not empty... {}".format(axis, self.command_buffer))
+            if self.command_buffer[axis] and self.ready_for_next_command[axis]:
+                print("starting next command for {} axis {}".format(self.target, axis))
+                print(self.command_buffer[axis])
+                self.axis_busy_state[axis] = True
+                print("axis {} for {} just became busy at {}".format(axis, self.target, ctime()))
+                self.ready_for_next_command[axis] = False
+                self.process_time[axis] = self.command_buffer[axis][0][1]
+                del self.command_buffer[axis][0]
+                print("Command complete for {} axis {}".format(self.target, axis))
+                print("Command buffer: ", self.command_buffer)
+                print("\n starting new {} task for duration {} at time {}\n".format(self.target,
+                                                                                    self.process_time[axis],
+                                                                                    ctime()))
+                sleep(self.process_time[axis])
+                self.ready_for_next_command[axis] = True
+                print("remaining command buffer: ", self.command_buffer)
+                if not self.command_buffer[axis]:
+                    self.axis_busy_state[axis] = False
+                    print("axis {} for {} just became free at {}".format(axis, self.target, ctime()))
 
 
-    def ax3_server(self):
-        while True:
-            if self.command_buffer_ax3 and self.ready_for_next_command_ax3:
-                self.busy_ax3 = True
-                self.ready_for_next_command_ax3 = False
-                self.process_time_ax3 = self.command_buffer_ax3[0][1]
-                del self.command_buffer_ax3[0]
-                print(
-                    "\nStarting new {} task for duration {} at time {}\n".format(self.target, self.process_time_ax3, ctime()))
-                sleep(self.process_time_ax3)
-                self.ready_for_next_command_ax3 = True
-                if not self.command_buffer_ax3:
-                    self.busy_ax3 = False
-
-
-    def newPrintThread(self):
-        while True:
-            if self.command_buffer and self.ready_for_next_command:
-                self.busy = True
-                self.ready_for_next_command = False
-                self.print_time = self.command_buffer[0][1]
-                del self.command_buffer[0]
-                print(
-                    "\nStarting new {} task for duration {} at time {}\n".format(self.target, self.print_time, ctime()))
-                sleep(self.print_time)
-                self.ready_for_next_command = True
-                if not self.command_buffer:
-                    self.busy = False
-
-
-channel1 = Serial()
+channel1 = Serial(port="COM4")
 print("Opening channel 1 and sending print: ", ctime())
-channel1.write("!99233")
+channel1.write("!9923301")
 unused = channel1.readline()
-print(unused)
-print("Print started: ", ctime(), "\n")
+print("Returned message: ", unused)
 
-channel2 = Serial()
+
+channel1.write("!9923304")
+unused = channel1.readline()
+print("Returned message: ", unused)
+
+channel1.write("!9923304")
+unused = channel1.readline()
+print("Returned message: ", unused)
+
+channel2 = Serial(port="COM3")
 print("Opening channel 2 and sending print: ", ctime())
 channel2.write(":01060D001008D4\r\n")
 unused = channel2.readline()
-print(unused)
-print("Print started: ", ctime(), "\n")
+print("Returned message: ", unused)
+
 channel2.write(":01060D001008D4\r\n")
 unused = channel2.readline()
-print(unused)
 print("Adding 2nd PAL command to channel 2 buffer: ", ctime(), "\n")
+print("Returned message: ", unused)
+
 channel2.write(":01060D001000DC\r\n")
 unused = channel2.readline()
-print(unused)
 print("Adding 3rd PAL command to channel 2 buffer: ", ctime(), "\n")
+print("Returned message: ", unused)
 
-channel1.write("!99212")
-busy1 = channel1.readline()
+
+channel1.write("!9921201")
+busy1a = channel1.readline()
+channel1.write("!9921202")
+busy1b = channel1.readline()
+channel1.write("!9921204")
+busy1c = channel1.readline()
 channel2.write(":01039007000164\r\n")
 busy2 = channel2.readline()
-while (busy1 or busy2):
-    print("Busy: {} ... Channel 1: {}... Channel 2: {}".format(ctime(), busy1, busy2))
+while (busy1a or busy1b or busy1c or busy2):
+    print("Busy: {} ... Channel 1a: {}... Channel 1b: {}... Channel 1c: {}... Channel 2: {}".format(
+        ctime(), busy1a, busy1b, busy1c, busy2))
     sleep(0.5)
-    channel1.write("!99212")
-    busy1 = channel1.readline()
+    channel1.write("!9921201")
+    busy1a = channel1.readline()
+    channel1.write("!9921202")
+    busy1b = channel1.readline()
+    channel1.write("!9921204")
+    busy1c = channel1.readline()
     channel2.write(":01039007000164\r\n")
     busy2 = channel2.readline()
 
 print("Ended: ", ctime())
 print(channel2.tt_commands["234"])
-
